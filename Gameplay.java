e55b0636beb368ba5939b13efb2a84eeb07da5dc
Tony <johnanthonypicadizofrancisco@gmail.com>
2019-03-19 02:12:51 +0800

    added collision in the ball and bricks

--- Gameplay.java
+++ Gameplay.java
@@ -1,4 +1,3 @@
-package breakThisMathafacker;
 
 import java.awt.event.KeyListener;
 import java.awt.event.ActionListener;
@@ -73,6 +72,34 @@ public class Gameplay extends JPanel implements KeyListener, ActionListener {
 			if(new Rectangle(ballPosX, ballPosY, 20, 20).intersects(new Rectangle(playerX, 550, 100, 8))) {
 				ballYdir = -ballYdir;
 			}
+			
+			A: for(int i = 0; i < map.map.length; i++) {
+				for( int j = 0; j < map.map[0].length; j++) {
+					if(map.map[i][j] > 0) {
+						int brickX = j* map.brickWidth + 90;
+						int brickY = i*map.brickHeight + 50;
+						int brickWidth = map.brickWidth;
+						int brickHeight = map.brickHeight;
+						
+						Rectangle rect = new Rectangle(brickX, brickY, brickWidth, brickHeight);
+						Rectangle ballRect = new Rectangle(ballPosX, ballPosY, 20, 20);
+						Rectangle brickRect = rect;
+						
+						if(ballRect.intersects(brickRect)) {
+							map.setBrickValue(0, i, j);
+							totalBricks--;
+							score += 5;
+							
+							if(ballPosX + 19 <= brickRect.x || ballPosX + 1 >= brickRect.x + brickRect.width) {
+								ballXdir = -ballXdir;
+							}else {
+								ballYdir = -ballYdir;
+							}
+							break A;
+						}
+					}
+				}
+			}	
 			
 			ballPosX += ballXdir;
 			ballPosY += ballYdir;
